c6d3498ac9ec30914cfaf5c36e22abd1e4cf426b
Vipin Patil <vipinpatil01@gmail.com>
2025-04-20 14:05:44 +0530

    Init

---
+++ Ditto/src/test/java/com/jsk/ditto/Ditto/DittoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsk.ditto.Ditto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DittoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
